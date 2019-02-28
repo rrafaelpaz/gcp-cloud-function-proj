c561b22174a9ee11d031cbdc24b368421c3edc19
Rafael Paz <rrafaelpaz@gmail.com>
2019-02-28 14:19:15 +1100

    Changes

--- application/main.py
+++ application/main.py
@@ -1,7 +1,7 @@
 
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import LoginForm
+from application import app
+from application.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
