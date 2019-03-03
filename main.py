6a907b729b0eb6a05f4d6e986e677b1121fa9e46
Rafael Paz <rrafaelpaz@gmail.com>
2019-03-04 07:50:43 +1100

    testing

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def test(request):
 
 #@app.route('/test3')
 def test3(request):
-    cmd = "python ./my-proxy/tools/deploy.py -n rafael_proxy -u rrafaelpaz@gmail.com:!Cranberries@2018 -o rrafaelpaz-eval -e test -d ./my-proxy -p /"
+    cmd = "python deploy.py -n rafael_proxy -u rrafaelpaz@gmail.com:!Cranberries@2018 -o rrafaelpaz-eval -e test -d ./my-proxy -p /"
     p = subprocess.Popen(cmd , shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=False)
     output = b''.join(p.stdout).decode('utf-8')
     #return subprocess.check_call(["./my-proxy/tools/deploy.py", 'rafael_proxy', 'rrafaelpaz@gmail.com:!Cranberries@2018', 'rrafaelpaz-eval', 'test', './my-proxy', '/' ])
